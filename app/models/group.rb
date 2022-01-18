33560c2e143027bfdb3ff91ead4ff96ef76fc92b
yassboss <kyh58221022@gmail.com>
2022-01-18 23:57:22 +0900

    コントローラー、ビューの作成

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,8 @@
 class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
+
+  with_options presence: true do
+    validates :name, :introduction, :owner_id
+  end
 end
