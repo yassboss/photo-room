47d95e881b27dc6cdc76019ab2eacdacae92779a
yassboss <kyh58221022@gmail.com>
2022-01-29 21:25:30 +0900

    基本の投稿機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,32 @@ class PostsController < ApplicationController
     @group_ids = GroupUser.select(:group_id).where(user_id: current_user.id) if user_signed_in?
     @user_groups = Group.where(id: @group_ids)
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text, :action, {images: []}).merge(user_id: current_user.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
   resources :groups, only: [:new, :create, :show, :edit, :update ]
+  resources :posts
 end
