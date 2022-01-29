50284a32354fe116ab30e8d9c4ab038cd6de94f0
yassboss <kyh58221022@gmail.com>
2022-01-29 23:56:21 +0900

    個人写真投稿詳細ページの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,13 @@ class PostsController < ApplicationController
     end
   end
 
+  def show
+    @post = Post.find(params[:id])
+
+    @group_ids = GroupUser.select(:group_id).where(user_id: current_user.id) if user_signed_in?
+    @user_groups = Group.where(id: @group_ids)
+  end
+
   def update
   end
 
