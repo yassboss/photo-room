7db38cdf7b3031abca0f13e9df58da6afe353544
yassboss <kyh58221022@gmail.com>
2022-02-12 12:43:08 +0900

    Update group_post.rb

--- app/models/group_post.rb
+++ app/models/group_post.rb
@@ -1,4 +1,5 @@
 class GroupPost < ApplicationRecord
-  has_many :posts
+  has_many :posts, dependent: :destroy
+  accepts_nested_attributes_for :posts, allow_destroy: true, reject_if: :all_blank
   belongs_to :group
 end
