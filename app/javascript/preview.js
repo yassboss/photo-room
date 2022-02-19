38dd824eec83ec098b2481aa52b38e61e1c59101
yassboss <kyh58221022@gmail.com>
2022-02-19 23:10:09 +0900

    グループ投稿ビュー修正

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -102,8 +102,6 @@ document.addEventListener('DOMContentLoaded', function(){
 
   // input要素を取得
   const fileField = document.querySelector('input[type="file"][name="post[images][]"]')
-  // const fileField = document.querySelector('input[type="file"]');
-  // const num = fileField.getAttributes("name");
   
   // input要素で値の変化が起きた際に呼び出される関数
   fileField.addEventListener('change', changedFileField);
