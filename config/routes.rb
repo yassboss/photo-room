703a8f7423c17e969a08f6ed43dc8b6653a7864b
yassboss <kyh58221022@gmail.com>
2022-02-20 22:08:43 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
   resources :groups, only: [:new, :create, :show, :edit, :update ]
-  resources :posts
-  resources :group_posts, only: [:create, :show, :edit, :update, :destroy]
+  resources :posts do
+    resources :comments, only: [:create]
+  end
+  resources :group_posts, only: [:create, :show, :edit, :update, :destroy] do
+    resources :comments, only: [:create]
+  end
   get 'group_posts/new/:id', to: 'group_posts#new', as: :new_group_post
 end
