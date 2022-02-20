af46f4549b4bb65fcb74932f54bb2e505857aa6a
yassboss <kyh58221022@gmail.com>
2022-02-20 15:52:20 +0900

    アソシエーション記述

--- app/models/group_post.rb
+++ app/models/group_post.rb
@@ -1,5 +1,6 @@
 class GroupPost < ApplicationRecord
   has_many :posts, dependent: :destroy
+  has_many :comments, as: :commentlable
   accepts_nested_attributes_for :posts, allow_destroy: true, reject_if: :all_blank
   belongs_to :group
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many_attached :images, dependent: :destroy
+  has_many :comments, as: :commentlable
 
   validates :title, presence: true
   validates :text, presence: true
