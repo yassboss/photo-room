45be13d1c71aa13082c4a0774e4e7d741b5532b4
yassboss <kyh58221022@gmail.com>
2022-03-21 21:49:51 +0900

    グループ詳細ページのビュー修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,6 @@ class Post < ApplicationRecord
   has_many :comments, as: :commentable, dependent: :destroy
   validates :title, presence: true
   validates :text, presence: true
-  validates :group_post_id, presence: true
   validates :images, presence: true
   validates :images, length: { minimum: 1, maximum: 5, message: 'は1枚以上5枚以下にしてください' }
   validates :user_id, presence: true
